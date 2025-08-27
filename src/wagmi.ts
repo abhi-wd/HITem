e83931ff71f566f3e129607c452139d60aaccf64
Abhishek Gupta <abhishekgupta954@gmail.com>
2025-08-27 15:29:23 +0530

    first commit

---
+++ src/wagmi.ts
@@ -0,0 +1,19 @@
+import { http, createConfig } from 'wagmi'
+import { base } from 'wagmi/chains'
+import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
+
+export const config = createConfig({
+  chains: [base],
+  transports: {
+    [base.id]: http(),
+  },
+  connectors: [
+    miniAppConnector()
+  ]
+})
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
